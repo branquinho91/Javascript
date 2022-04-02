b4633abf3b41b4fb402cd727b0adac71d97bba42
Gustavo Branquinho <gustavobranquinho2@gmail.com>
2022-04-02 15:15:44 -0300

    commit

--- Case Converter/script.js
+++ Case Converter/script.js
@@ -17,31 +17,33 @@ saveTextFile.addEventListener("click", () => download(textArea.value));
 
 
 function toProperCase(arr) {
-    const words = arr.split(" ");
-    words.forEach((word, index) => { words[index] =
-        word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
-    });
-    return words.join(" ");
+  const words = arr.split(" ");
+  words.forEach((word, index) => {
+    words[index] =
+    word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
+  });
+  return words.join(" ");
 }
 
 function toSentenceCase(arr) {
-    const words = arr.split(". ");
-    words.forEach((word, index) => { words[index] =
-        word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
-    });
-    return words.join(". ");
+  const words = arr.split(". ");
+  words.forEach((word, index) => {
+    words[index] =
+    word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
+  });
+  return words.join(". ");
 }
 
 function download(text) {
 
-    let element = document.createElement("a");
-    element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
-    element.setAttribute("download", "text_converted.txt");
+  let element = document.createElement("a");
+  element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
+  element.setAttribute("download", "text_converted.txt");
 
-    element.style.display = "none";
-    document.body.appendChild(element);
+  element.style.display = "none";
+  document.body.appendChild(element);
 
-    element.click();
+  element.click();
 
-    document.body.removeChild(element);
+  document.body.removeChild(element);
 }
